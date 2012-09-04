cda7aaf572378722b076a8130825b21cd4955c34
Jasper Van der Jeugt <m@jaspervdj.be>
2012-09-04 13:39:36 +0900

    Update example

--- example.hs
+++ example.hs
@@ -5,28 +5,31 @@
 --------------------------------------------------------------------------------
 import           Control.Applicative            (pure, (<$>), (<*>), (<*))
 import           Control.Concurrent             (threadDelay)
-import           Control.Monad                  (forever)
+import           Control.Monad                  (forever, join)
 import           System.Environment             (getArgs)
 import           System.Random
 
 
 --------------------------------------------------------------------------------
 import           FRP.Elerea.Simple
-import           FRP.Euphoria.Network
-import           FRP.Euphoria.Network.UdpPool
+import qualified FRP.Euphoria.Network as Network
 
 
 --------------------------------------------------------------------------------
-server :: String -> Int -> IO ()
-server host port = do
-    sender <- mkNetwork "0.0.0.0" 123456
-    gen    <- newStdGen
-    peer   <- mkPeer host port
+server :: IO ()
+server = do
+    gen <- newStdGen
 
     sampler <- start $ do
         r <- randomSignal gen
-        s <- networkBroadcast sender [peer] 0 (+) r
-        return s
+
+        rec
+            clientsGen <- execute $
+                Network.server "0.0.0.0" 123456 (0 :: Int) out
+            clients <- delay [] =<< clientsGen
+            let out = (\cs d -> zip cs (repeat d)) <$> clients <*> r
+
+        return out
 
     forever $ do
         x <- sampler
@@ -46,16 +49,17 @@ randomSignal gen = do
 
 
 --------------------------------------------------------------------------------
-client :: String -> Int -> IO ()
-client host port = do
-    network <- mkNetwork host port
-    sampler <- start $ networkReceive network 0 (+) :: IO (IO [(Peer, Int)])
+client :: IO ()
+client = do
+    sampler <- start $
+        join $ execute $ Network.client "127.0.0.1" 123456 (0 :: Int) (+)
     forever $ do
         x <- sampler
         putStrLn $ "Client generated sample: " ++ show x
         threadDelay $ 1000 * 1000
 
 
+{-
 --------------------------------------------------------------------------------
 -- | The client computes a weighted average
 average :: Signal Int -> SignalGen (Signal Double)
@@ -67,6 +71,7 @@ average signal = do
     return averages
   where
     update w x = w * 0.9 + fromIntegral x * 0.1
+-}
 
 
 --------------------------------------------------------------------------------
@@ -74,6 +79,6 @@ main :: IO ()
 main = do
     args <- getArgs
     case args of
-        ("server" : _) -> server "127.0.0.1" 12345
-        ("client" : _) -> client "0.0.0.0" 12345
+        ("server" : _) -> server
+        ("client" : _) -> client
         _              -> error "Specify either 'server' or 'client'"
--- src/FRP/Euphoria/Network.hs
+++ src/FRP/Euphoria/Network.hs
@@ -1,149 +1,98 @@
 --------------------------------------------------------------------------------
-{-# LANGUAGE DoRec                      #-}
-{-# LANGUAGE ExistentialQuantification  #-}
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
-{-# LANGUAGE ScopedTypeVariables        #-}
+{-# LANGUAGE DoRec #-}
 module FRP.Euphoria.Network
-    ( Network
-    , mkNetwork
-    , networkPeers
-    , networkReceive
-    , networkSend
-    , networkBroadcast
+    ( client
+    , server
     ) where
 
 
 --------------------------------------------------------------------------------
-import           Control.Applicative             ((<$>), (<*>), (*>))
-import           Control.Concurrent              (forkIO)
-import           Control.Monad                   (forM, void)
+import           Control.Applicative         ((*>), (<$>), (<*>))
+import           Control.Concurrent          (forkIO)
+import           Control.Monad               (forM, forM_, void)
 import           Data.IORef
-import           Data.Map                        (Map)
-import qualified Data.Map                        as M
-import           Data.Serialize                  (Serialize)
-import qualified Data.Serialize                  as Serialize
-import           Data.Typeable                   (Typeable)
+import           Data.Map                    (Map)
+import qualified Data.Map                    as M
+import           Data.Serialize              (Serialize)
+import           Data.Serialize              (Serialize)
+import qualified Data.Serialize              as Serialize
+import qualified Data.Serialize              as Serialize
+import           Data.Typeable               (Typeable)
 import           FRP.Elerea.Simple
 
 
 --------------------------------------------------------------------------------
-import           FRP.Euphoria.Network.Connection
-import           FRP.Euphoria.Network.Types
-import           FRP.Euphoria.Network.UdpPool
+import           FRP.Euphoria.Network.Packet
+import           FRP.Euphoria.Network.Socket
 
 
 --------------------------------------------------------------------------------
-data Network = Network
-    { networkUdpPool     :: UdpPool
-    , networkConnections :: IORef (Map Peer Connection)
-    }
+client :: (Serialize is, Serialize id)
+       => String
+       -> Int
+       -> is
+       -> (id -> is -> is)
+       -> IO (SignalGen (Signal is))
+client host port initialIn updateIn = do
+    (packetsInGen, packetIn) <- externalMulti
+    c <- runClient host port packetIn
+
+    return $ do
+        packetsIn <- packetsInGen
+        statesIn  <- foldSignal
+            (flip $ foldr $ updateWithPacket updateIn) initialIn packetsIn
+
+        return statesIn
 
 
 --------------------------------------------------------------------------------
-mkNetwork :: String -> Int -> IO Network
-mkNetwork host port = do
-    pool        <- mkUdpPool host port
-    connections <- newIORef M.empty
-
-    let pool' = udpPoolSetHandlers (connect_ nw) (disconnect nw) pool
-        nw    = Network pool' connections
-
-    _ <- forkIO $ receiveLoop nw
-    return nw
-
-
---------------------------------------------------------------------------------
-receiveLoop :: Network -> IO ()
-receiveLoop nw = do
-    putStrLn "Reading..."
-    (peer, bs) <- udpPoolRecv $ networkUdpPool nw
-    putStrLn $ "[" ++ show peer ++ "]: " ++ show bs
-
-    case Serialize.decode bs of
-        Left err     -> error $ "Can't parse packet: " ++ err
-        Right packet -> do
-            conns <- readIORef (networkConnections nw)
-            case M.lookup peer conns of
-                Nothing   -> error "Unknown peer!"  -- Should not happen
-                Just conn -> connectionQueue conn packet
-
-    receiveLoop nw
-
-
---------------------------------------------------------------------------------
-networkPeers :: Network -> SignalGen (Signal [Peer])
-networkPeers = effectful . udpPoolPeers . networkUdpPool
-
-
---------------------------------------------------------------------------------
-networkReceive
-    :: forall s d. (Serialize s, Serialize d, Typeable s, Typeable d)
-    => Network
-    -> s
-    -> (d -> s -> s)
-    -> SignalGen (Signal [(Peer, s)])
-networkReceive nw initial update = effectful flushAll
+server :: (Serialize os, Serialize od)
+       => String
+       -> Int
+       -> os
+       -> Signal [(Client, od)]
+       -> IO (SignalGen (Signal [Client]))
+server host port initialOut deltasOut = do
+    s <- runServer host port onConnect onDisconnect onPacket
+    return $ do
+        out     <- effectful1 sendDeltasOut deltasOut
+        clients <- effectful $ serverGetClients s
+        return $ out *> clients
   where
-    flushAll = do
-        conns <- readIORef (networkConnections nw)
-        forM (M.toList conns) $ \(peer, conn) -> do
-            x <- connectionFlush conn initial update
-            return (peer, x)
+    onConnect c = do
+        putStrLn $ show c ++ " disconnected"
+        clientSend c $ Packet AbsolutePacket (Serialize.encode initialOut)
+
+    onDisconnect c = putStrLn $ show c ++ " disconnected"
+
+    onPacket _ _ = return ()
+
+    sendDeltasOut ds = forM_ ds $ \(c, d) ->
+        clientSend c $ Packet DeltaPacket (Serialize.encode d)
 
 
 --------------------------------------------------------------------------------
-networkSend :: (Serialize s, Serialize d, Typeable s, Typeable d)
-            => Network
-            -> Signal [(Peer, Update s d)]
-            -> SignalGen (Signal [(Peer, s)])
-networkSend nw peerS = effectful1 send peerS
-  where
-    send peers = do
-        conns <- readIORef (networkConnections nw)
-        forM peers $ \(peer, update) -> do
-            conn  <- maybe (connect nw peer) return $ M.lookup peer conns
-            state <- connectionSend conn update
-            return (peer, state)
-
-
---------------------------------------------------------------------------------
-networkBroadcast
-    :: forall s d. (Serialize s, Serialize d, Typeable s, Typeable d)
-    => Network
-    -> [Peer]
-    -> s
-    -> (d -> s -> s)
-    -> Signal d
-    -> SignalGen (Signal s)
-networkBroadcast nw peers initial update deltaS = do
+foldSignal :: (a -> b -> b) -> b -> Signal a -> SignalGen (Signal b)
+foldSignal f initial x = do
     rec
-        updates <- delay (Absolute initial) updates'
-        let updates' = update' <$> deltaS <*> updates
+        let acc = f <$> x <*> acc'
+        acc' <- delay initial acc
 
-    out <- networkSend nw $ zip peers . repeat <$> updates 
-    return $ out *> fmap updateState updates
-  where
-    update' :: d -> Update s d -> Update s d
-    update' d = Delta d . update d . updateState
+    return acc
 
 
 --------------------------------------------------------------------------------
-connect :: Network -> Peer -> IO Connection
-connect nw peer = do
-    putStrLn $ "Connected: " ++ show peer
-    conn <- mkConnection $ \bs -> udpPoolSend (networkUdpPool nw) peer bs
-    atomicModifyIORef (networkConnections nw) $ \m ->
-        (M.insert peer conn m, ())
-    return conn
-
-
---------------------------------------------------------------------------------
-connect_ :: Network -> Peer -> IO ()
-connect_ nw = void . connect nw
-
-
---------------------------------------------------------------------------------
-disconnect :: Network -> Peer -> IO ()
-disconnect nw peer = do
-    putStrLn $ "Disconnected: " ++ show peer
-    atomicModifyIORef (networkConnections nw) $ \m -> (M.delete peer m, ())
+updateWithPacket
+    :: (Serialize s, Serialize d)
+    => (d -> s -> s)  -- ^ Delta updater
+    -> Packet         -- ^ Packet with updating payload
+    -> s              -- ^ Initial state
+    -> s              -- ^ Resulting state
+updateWithPacket update packet initial = case packetType packet of
+    AckPacket      -> initial  -- But this should not happen
+    AbsolutePacket -> case Serialize.decode (packetData packet) of
+        Left  err      -> error err  -- TODO return the initial state?
+        Right newstate -> newstate
+    DeltaPacket    -> case Serialize.decode (packetData packet) of
+        Left err    -> error err  -- TODO probably return initial state
+        Right delta -> update delta initial
